a002d3c21aaac8988ebb59c556c790666f269f52
vanderschuea <antoine.vanderschueren@student.uclouvain.be>
2018-08-30 13:18:02 +0200

    python => python3 in train.sh

--- config/config.py
+++ config/config.py
@@ -34,7 +34,7 @@ kernel_lr_multiplier = 10
 
 # debug and logging
 progress_logging = 1 # can be 0 = no std logging, 1 = progress bar logging, 2 = one log line per epoch
-epochs = 100
+epochs = 1
 batch_size = 64
 lr = 0.001
 decay = 0.000025
