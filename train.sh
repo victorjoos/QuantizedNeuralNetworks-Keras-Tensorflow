69a7ece4f8f8ec3fd87ad15fa32016f2a1d70af2
vanderschuea <antoine.vanderschueren@student.uclouvain.be>
2018-09-24 15:33:08 +0200

    added LR to script

--- train.sh
+++ train.sh
@@ -2,32 +2,33 @@
 # cuDNN
 
 NRES=$1
-GPU=$2
+LR=$2
+GPU=$3
 CONFIG=config
 TIME=$(date +"%Y-%m-%d_%H-%M-%S")
 mkdir "logs/$TIME"
 mkdir "$TIME"
 # standard float
-# python3 train.py -c "$CONFIG" -o  nres="$NRES" date="$TIME" cuda="$GPU" network_type='float' | tee "logs/$TIME/ff.out"
+# python3 train.py -c "$CONFIG" -o date="$TIME" lr="$LR" nres="$NRES" cuda="$GPU" network_type='float' | tee "logs/$TIME/ff.out"
 
 # binary
-python3 train.py -c "$CONFIG" -o date="$TIME" nres="$NRES" cuda="$GPU" network_type='full-bnn' | tee "logs/$TIME/bb.out"
-python3 train.py -c "$CONFIG" -o date="$TIME" nres="$NRES" cuda="$GPU" network_type='qbnn' abits=2 | tee "logs/$TIME/b2.out"
-python3 train.py -c "$CONFIG" -o date="$TIME" nres="$NRES" cuda="$GPU" network_type='qbnn' abits=4 | tee "logs/$TIME/b4.out"
-python3 train.py -c "$CONFIG" -o date="$TIME" nres="$NRES" cuda="$GPU" network_type='qbnn' abits=8 | tee "logs/$TIME/b8.out"
-# python3 train.py -c "$CONFIG" -o date="$TIME" nres="$NRES" cuda="$GPU" network_type='bnn' abits=8 | tee "logs/$TIME/bf.out"
+python3 train.py -c "$CONFIG" -o date="$TIME" lr="$LR" nres="$NRES" cuda="$GPU" network_type='full-bnn' | tee "logs/$TIME/bb.out"
+python3 train.py -c "$CONFIG" -o date="$TIME" lr="$LR" nres="$NRES" cuda="$GPU" network_type='qbnn' abits=2 | tee "logs/$TIME/b2.out"
+python3 train.py -c "$CONFIG" -o date="$TIME" lr="$LR" nres="$NRES" cuda="$GPU" network_type='qbnn' abits=4 | tee "logs/$TIME/b4.out"
+python3 train.py -c "$CONFIG" -o date="$TIME" lr="$LR" nres="$NRES" cuda="$GPU" network_type='qbnn' abits=8 | tee "logs/$TIME/b8.out"
+# python3 train.py -c "$CONFIG" -o date="$TIME" lr="$LR" nres="$NRES" cuda="$GPU" network_type='bnn' abits=8 | tee "logs/$TIME/bf.out"
 
 
 # ternary
-python3 train.py -c "$CONFIG" -o date="$TIME" nres="$NRES" cuda="$GPU" network_type='full-tnn' | tee "logs/$TIME/tt.out"
-python3 train.py -c "$CONFIG" -o date="$TIME" nres="$NRES" cuda="$GPU" network_type='qtnn' abits=2 | tee "logs/$TIME/t2.out"
-python3 train.py -c "$CONFIG" -o date="$TIME" nres="$NRES" cuda="$GPU" network_type='qtnn' abits=4 | tee "logs/$TIME/t4.out"
-python3 train.py -c "$CONFIG" -o date="$TIME" nres="$NRES" cuda="$GPU" network_type='qtnn' abits=8 | tee "logs/$TIME/t8.out"
-# python3 train.py -c "$CONFIG" -o date="$TIME" nres="$NRES" cuda="$GPU" network_type='tnn' abits=8 | tee "logs/$TIME/tf.out"
+python3 train.py -c "$CONFIG" -o date="$TIME" lr="$LR" nres="$NRES" cuda="$GPU" network_type='full-tnn' | tee "logs/$TIME/tt.out"
+python3 train.py -c "$CONFIG" -o date="$TIME" lr="$LR" nres="$NRES" cuda="$GPU" network_type='qtnn' abits=2 | tee "logs/$TIME/t2.out"
+python3 train.py -c "$CONFIG" -o date="$TIME" lr="$LR" nres="$NRES" cuda="$GPU" network_type='qtnn' abits=4 | tee "logs/$TIME/t4.out"
+python3 train.py -c "$CONFIG" -o date="$TIME" lr="$LR" nres="$NRES" cuda="$GPU" network_type='qtnn' abits=8 | tee "logs/$TIME/t8.out"
+python3 train.py -c "$CONFIG" -o date="$TIME" lr="$LR" nres="$NRES" cuda="$GPU" network_type='tnn' abits=8 | tee "logs/$TIME/tf.out"
 
 
 # qnn
-python3 train.py -c "$CONFIG" -o nres="$NRES" date="$TIME" cuda="$GPU" network_type='full-qnn' wbits=4 abits=2 | tee "logs/$TIME/42.out"
-python3 train.py -c "$CONFIG" -o nres="$NRES" date="$TIME" cuda="$GPU" network_type='full-qnn' wbits=4 abits=4 | tee "logs/$TIME/44.out"
-python3 train.py -c "$CONFIG" -o nres="$NRES" date="$TIME" cuda="$GPU" network_type='full-qnn' wbits=4 abits=8 | tee "logs/$TIME/48.out"
-# python3 train.py -c "$CONFIG" -o nres="$NRES" date="$TIME" cuda="$GPU" network_type='qnn' wbits=4 | tee "$TIME/4f.out"
+python3 train.py -c "$CONFIG" -o date="$TIME" lr="$LR" nres="$NRES" cuda="$GPU" network_type='full-qnn' wbits=4 abits=2 | tee "logs/$TIME/42.out"
+python3 train.py -c "$CONFIG" -o date="$TIME" lr="$LR" nres="$NRES" cuda="$GPU" network_type='full-qnn' wbits=4 abits=4 | tee "logs/$TIME/44.out"
+python3 train.py -c "$CONFIG" -o date="$TIME" lr="$LR" nres="$NRES" cuda="$GPU" network_type='full-qnn' wbits=4 abits=8 | tee "logs/$TIME/48.out"
+# python3 train.py -c "$CONFIG" -o date="$TIME" lr="$LR" nres="$NRES" cuda="$GPU" network_type='qnn' wbits=4 | tee "$TIME/4f.out"
