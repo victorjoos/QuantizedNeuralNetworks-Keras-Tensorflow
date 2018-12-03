8656b4afd64f6520cffaa037c0f360ed41036805
vanderschuea <antoine.vanderschueren@student.uclouvain.be>
2018-12-03 15:54:59 +0100

    added 2-bit weights

--- global.sh
+++ global.sh
@@ -1,8 +1,9 @@
 NRES=$1
 GPU=$2
+PFI=$3
 
-./train.sh "$NRES" 0.001 "$GPU"
-./train.sh "$NRES" 0.005 "$GPU"
-./train.sh "$NRES" 0.01 "$GPU"
-./train.sh "$NRES" 0.02 "$GPU"
-./train.sh "$NRES" 0.1 "$GPU"
+./train.sh "$NRES" 0.001 "$GPU" "$PFI"
+./train.sh "$NRES" 0.005 "$GPU" "$PFI"
+./train.sh "$NRES" 0.01 "$GPU" "$PFI"
+./train.sh "$NRES" 0.02 "$GPU" "$PFI"
+./train.sh "$NRES" 0.1 "$GPU" "$PFI"
