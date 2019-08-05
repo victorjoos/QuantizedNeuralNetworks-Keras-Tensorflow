9061b69f55be85b6e236a9a514c0647ed3feeab7
vanderschuea <antoine.vanderschueren@uclouvain.be>
2019-08-05 16:01:32 +0200

    Added some possible model-mixes

--- models/model_factory.py
+++ models/model_factory.py
@@ -9,12 +9,14 @@ from layers.quantized_layers import QuantizedConv2D,QuantizedDense
 from layers.quantized_ops import quantized_tanh as quantize_op
 from layers.binary_layers import BinaryConv2D, BinaryDense
 from layers.binary_ops import binary_tanh
+from layers.binary_ops import BinaryReLU
 from layers.binary_ops import binary_separation
 from layers.ternary_layers import TernaryConv2D, TernaryDense
 from layers.ternary_ops import ternary_tanh
 
 from models.resnet import ResNet18
 from models.vgg import Vgg
+from models.mlp import Mlp
 
 def build_model(cf):
     def quantized_relu(x):
@@ -22,17 +24,26 @@ def build_model(cf):
 
 
     H = 1.
-    if cf.network_type =='float':
+    if cf.network_type in ['float', 'float-bnn', 'float-tnn']:
         Conv = Conv2D
         Fc = Dense
-        Act = lambda: LeakyReLU()
+        if cf.network_type == 'float-bnn':
+            Act = lambda: BinaryReLU()
+        elif cf.network_type == 'float-tnn':
+            Act = lambda: Activation(ternary_tanh)
+        else:
+            Act = lambda: LeakyReLU()
 
-    elif cf.network_type in ['qnn', 'full-qnn']:
+    elif cf.network_type in ['qnn', 'full-qnn', 'bqnn', 'tqnn']:
         Conv = lambda **kwargs: QuantizedConv2D(H=1, nb=cf.wbits, **kwargs)
-        Fc = lambda **kwargs: QuantizedDense(nb=cf.abits, **kwargs)
+        Fc = lambda **kwargs: QuantizedDense(nb=cf.wbits, **kwargs)
 
         if cf.network_type=='qnn':
             Act = lambda: LeakyReLU()
+        elif cf.network_type=='bqnn':
+            Act = lambda: BinaryReLU()
+        elif cf.network_type=='tqnn':
+            Act = lambda: Activation(ternary_tanh)
         else: # full-qnn
             Act = lambda: Activation(quantized_relu)
 
@@ -45,7 +56,7 @@ def build_model(cf):
         elif cf.network_type=='qbnn':
             Act = lambda: Activation(quantized_relu)
         else: #full-bnn
-            Act = lambda: Activation(binary_tanh)
+            Act = lambda: BinaryReLU()
 
     elif cf.network_type in ['tnn', 'qtnn', 'full-tnn', 'btnn']:
         Conv = lambda **kwargs: TernaryConv2D(H=1, **kwargs)
@@ -56,7 +67,7 @@ def build_model(cf):
         elif cf.network_type=='qtnn':
             Act = lambda: Activation(quantized_relu)
         elif cf.network_type=='btnn':
-            Act = lambda: Activation(binary_separation)
+            Act = lambda: BinaryReLU()
         else: #full-tnn
             Act = lambda: Activation(ternary_tanh)
 
@@ -67,6 +78,8 @@ def build_model(cf):
         model = Vgg(Conv, Act, Fc, cf)
     elif cf.architecture=="RESNET":
         model = ResNet18(Conv, Act, Fc, cf)
+    elif cf.architecture=="MLP":
+        model = Mlp(Act, Fc, cf)
     else:
         raise ValueError("Error: type " + str(cf.architecture) + " is not supported")
 
@@ -75,34 +88,3 @@ def build_model(cf):
     return model
 
 
-
-
-def load_weights(model, weight_reader):
-    weight_reader.reset()
-
-    for i in range(len(model.layers)):
-        if 'conv' in model.layers[i].name:
-            if 'batch' in model.layers[i + 1].name:
-                norm_layer = model.layers[i + 1]
-                size = np.prod(norm_layer.get_weights()[0].shape)
-
-                beta = weight_reader.read_bytes(size)
-                gamma = weight_reader.read_bytes(size)
-                mean = weight_reader.read_bytes(size)
-                var = weight_reader.read_bytes(size)
-
-                weights = norm_layer.set_weights([gamma, beta, mean, var])
-
-            conv_layer = model.layers[i]
-            if len(conv_layer.get_weights()) > 1:
-                bias = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[1].shape))
-                kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
-                kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
-                kernel = kernel.transpose([2, 3, 1, 0])
-                conv_layer.set_weights([kernel, bias])
-            else:
-                kernel = weight_reader.read_bytes(np.prod(conv_layer.get_weights()[0].shape))
-                kernel = kernel.reshape(list(reversed(conv_layer.get_weights()[0].shape)))
-                kernel = kernel.transpose([2, 3, 1, 0])
-                conv_layer.set_weights([kernel])
-    return model
